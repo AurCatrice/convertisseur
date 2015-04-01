61371b1709cfe506c6d3520a0cfa5df0f22affcf
Aurelia Catrice <catricea9@gmail.com>
2015-04-01 09:56:35 +0200

    20 test vert

--- src/main/java/iut/tdd/Convert.java
+++ src/main/java/iut/tdd/Convert.java
@@ -1,10 +1,32 @@
 package iut.tdd;
 
 public class Convert {
-	public static String num2text(String input) {
-		return null;
+	
+	public static String num2text(String input){
+		if(input.equals("0")){return "zéro";}
+		if(input.equals("1")){return "un";}
+		if(input.equals("2")){return "deux";}
+		if(input.equals("3")){return "trois";}
+		if(input.equals("4")){return "quatre";}
+		if(input.equals("5")){return "cinq";}
+		if(input.equals("6")){return "six";}
+		if(input.equals("7")){return "sept";}
+		if(input.equals("8")){return "huit";}
+		if(input.equals("9")){return "neuf";}
+		else{return null;}
 	}
-	public static String text2num(String input) {
-		return null;
+	public static String text2num(String input){
+		if(input.equals("zéro")){return "0";}
+		if(input.equals("un")){return "1";}
+		if(input.equals("deux")){return "2";}
+		if(input.equals("trois")){return "3";}
+		if(input.equals("quatre")){return "4";}
+		if(input.equals("cinq")){return "5";}
+		if(input.equals("six")){return "6";}
+		if(input.equals("sept")){return "7";}
+		if(input.equals("huit")){return "8";}
+		if(input.equals("neuf")){return "9";}
+		else{return null;}
+		
 	}
 }
--- src/test/java/iut/tdd/TestConvert.java
+++ src/test/java/iut/tdd/TestConvert.java
@@ -1,20 +1,108 @@
 package iut.tdd;
 
 import junit.framework.Assert;
-
 import org.junit.Test;
 
-public class TestConvert {
+public class TestConvert{
+	Convert myConvert= new Convert();
 	@Test
-	public void test_num2text_zero () {
-		// Given
-		String input = "0";
-		String expected = "zéro";
-		Convert myConvert = new Convert();
-		// When
-		String actual = myConvert.num2text(input);
-		// Then
-		Assert.assertEquals(expected, actual);
+	public void test_num2text_zero(){
+		String input="0"; String expected="zéro";
+		Assert.assertEquals(expected,myConvert.num2text(input));
 	}
-	
+	@Test
+	public void test_num2text_un(){
+		String input="1"; String expected="un";
+		Assert.assertEquals(expected,myConvert.num2text(input));
+	}
+	@Test
+	public void test_num2text_deux(){
+		String input="2"; String expected="deux";
+		Assert.assertEquals(expected,myConvert.num2text(input));
+	}
+	@Test
+	public void test_num2text_trois(){
+		String input="3"; String expected="trois";
+		Assert.assertEquals(expected,myConvert.num2text(input));
+	}
+	@Test
+	public void test_num2text_quatre(){
+		String input="4"; String expected="quatre";
+		Assert.assertEquals(expected,myConvert.num2text(input));
+	}
+	@Test
+	public void test_num2text_cinq(){
+		String input="5"; String expected="cinq";
+		Assert.assertEquals(expected,myConvert.num2text(input));
+	}
+	@Test
+	public void test_num2text_six(){
+		String input="6"; String expected="six";
+		Assert.assertEquals(expected,myConvert.num2text(input));
+	}	
+	@Test
+	public void test_num2text_sept(){
+		String input="7"; String expected="sept";
+		Assert.assertEquals(expected,myConvert.num2text(input));
+	}
+	@Test
+	public void test_num2text_huit(){
+		String input="8"; String expected="huit";
+		Assert.assertEquals(expected,myConvert.num2text(input));
+	}
+	@Test
+	public void test_num2text_neuf(){
+		String input="9"; String expected="neuf";
+		Assert.assertEquals(expected,myConvert.num2text(input));
+	}
+	@Test
+	public void test_text2num_0(){
+		String input="zéro"; String expected="0";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
+	@Test
+	public void test_text2num_1(){
+		String input="un"; String expected="1";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
+	@Test
+	public void test_text2num_2(){
+		String input="deux"; String expected="2";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
+	@Test
+	public void test_text2num_3(){
+		String input="trois"; String expected="3";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
+	@Test
+	public void test_text2num_4(){
+		String input="quatre"; String expected="4";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
+	@Test
+	public void test_text2num_5(){
+		String input="cinq"; String expected="5";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
+	@Test
+	public void test_text2num_6(){
+		String input="six"; String expected="6";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
+	@Test
+	public void test_text2num_7(){
+		String input="sept"; String expected="7";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
+	@Test
+	public void test_text2num_8(){
+		String input="huit"; String expected="8";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
+	@Test
+	public void test_text2num_9(){
+		String input="neuf"; String expected="9";
+		Assert.assertEquals(expected,myConvert.text2num(input));
+	}
 }
